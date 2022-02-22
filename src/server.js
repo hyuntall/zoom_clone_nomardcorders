ced64accc1012ee659490c6a436df952f784cfc7
hyuntall <hyunchul1998@naver.com>
2022-02-22 09:36:34 +0000

    change http to https

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import http from "http";
+import https from "https";
 import {Server} from "socket.io";
 import {instrument} from "@socket.io/admin-ui";
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
@@ -16,9 +18,17 @@ app.get("/", (req, res) => res.render("home"));
 // 다른 url 사용하지 않음
 app.get("/*", (req,res) => res.redirect("/"));
 
+//letsencrypt 인증서 경로
+const options = {
+	ca: fs.readFileSync('../fullchain.pem'),
+	key: fs.readFileSync('../privkey.pem'),
+	cert: fs.readFileSync('../cert.pem')
+};
+
 //websocket 서버 생성
 const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer, {
+const httpsServer = https.createServer(options, app);
+const wsServer = new Server(httpsServer, {
     cors: {
         origin: ["https://admin.socket.io"],
         credentials: true,
@@ -87,3 +97,4 @@ wsServer.on("connection", (socket) =>{
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
+httpsServer.listen(443, handleListen);
